d29f137ca5424860f3cdcb0a17ee3ed91540d34b
Cole Landers <jclanders@protonmail.com>
2024-11-26 14:05:37 -0600

    Moving things around, restructuring, change from bootstrap to custom sass

--- src/App.tsx
+++ src/App.tsx
@@ -1,27 +1,8 @@
 import "bootstrap/dist/js/bootstrap.min.js";
-import "./App.css";
-import "./assets/united.bootstrap.min.css";
-// import "bootstrap/dist/css/bootstrap.min.css";
-import { useState } from "react";
-import AnimeSearch from "./views/anime-search";
-import Nav from "./components/nav";
-import TrendingAnime from "./views/trending-anime";
+import "./assets/styles.scss";
 
 function App() {
-  const [view, setView] = useState("trending");
-  const handleNavChange = (navTo: string) => {
-    setView(navTo);
-  };
-
-  return (
-    <div className="d-flex justify-content-start">
-      <Nav onNavChange={handleNavChange} />
-      <main className="container">
-        {view === "search" && <AnimeSearch />}
-        {view === "trending" && <TrendingAnime />}
-      </main>
-    </div>
-  );
+  return <h1>Works</h1>;
 }
 
 export default App;
